9b4f2701fed9b002989f49a7f160765f3573793a
Matthew Richardson <mat_richardson@mac.com>
2021-06-29 12:14:40 +1000

    Initial Commit

---
+++ AnimatedProgress/AnimatedProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedProgressApp.swift
+//  AnimatedProgress
+//
+//  Created by Matthew Richardson on 29/6/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
